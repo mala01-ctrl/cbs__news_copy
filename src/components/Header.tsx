229b1a0e8b3a9d107249603eec1fe9bfc780822e
mala25ctrl <l.malavolti.proget@gmail.com>
2024-04-26 16:03:05 +0200

    feat(): Iniziata creazione dell'header della pagina inserendo bootstrap e fontawesome

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
+    const linkNav: string[] = ['Latest', 'Local News', 'Live', "Shows", "..."];
+
     return (
-        <header className="App-header">
-            <button>Modifica</button>
-        </header>
+        <>
+            <header className="App-header">
+                <div className="container-sm">
+                    <div className="row">
+                        <div className="col-sm">
+                            <nav>
+                                <ul>
+                                    {linkNav.map((link: string) => <li><a href={"#"}>{link}</a></li>)}
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="col-sm">
+                            <img src={""} alt={"logo immagine"}/>
+                        </div>
+                        <div className="col-sm" style={{textAlign: "right"}}>
+                            <i className="fa-solid fa-magnifying-glass" style={{fontSize: "30px"}}></i>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <header className="subHeader">
+                <div className="container sm">
+                    <nav>
+                        <ul>
+                            {linkNav.map((link: string) => <li><a href={"#"}>{link}</a></li>)}
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </>
     );
 };
 
